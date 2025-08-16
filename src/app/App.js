111a63dee351e3d09664e5ab99816a8b02fe3a02
Bhavin-Pathak  <bhavinpathak29@gmail.com>
2025-08-16 15:13:38 +0530

    diff: files

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../style/globals.css';
 
 function App() {
   return (
--- src/app/admin/loading.js
+++
@@ -1,4 +0,0 @@
-// This component is intentionally left empty to serve as a loading state.
-export default function Loading() {
-    return null
-}

e804882dfb809d7cf1d39e797356155bb2cc0c18
Bhavin-Pathak  <bhavinpathak29@gmail.com>
2025-08-18 15:56:07 +0530

    chore: adding css-utility for background

--- src/components/input.js
+++ src/components/input.js
@@ -7,7 +7,7 @@ const Input = React.forwardRef(({ className, type, ...props }, ref) => {
     <input
       type={type}
       className={cn(
-        "flex h-10 w-full rounded-md border border-gray-500 bg-transparent px-3 py-2 text-base text-foreground placeholder:text-muted-foreground focus:outline-none focus:border-primary disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "flex h-10 w-full rounded-md border border-gray-500 bg-transparent px-3 py-2 text-base text-foreground placeholder:text-muted-foreground focus:outline-none focus:border-primary disabled:cursor-not-allowed disabled:opacity-50 md:text-sm autofill:bg-transparent autofill:shadow-[inset_0_0_0px_1000px_transparent]",
         className
       )}
       ref={ref}

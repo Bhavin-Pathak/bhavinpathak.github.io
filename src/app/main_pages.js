44053c0f78970945e44dca8c891b3cca25a3e846
Bhavin-Pathak  <bhavinpathak29@gmail.com>
2025-08-18 17:12:23 +0530

    fix: imports

--- src/app/main_pages.js
+++ src/app/main_pages.js
@@ -1,4 +1,4 @@
-import Hero from "../pages/hero.js";
+import Hero from "../pages/home.js";
 import About from "../pages/about.js";
 import Skills from "../pages/skills.js";
 import Projects from "../pages/projects.js";
@@ -8,28 +8,28 @@ import Header from "../components/header.js";
 import Footer from "../components/footer.js";
 
 export default function Main_Pages() {
-    return (
-        <div className="min-h-screen">
-            <Header />
-            <section id="home" className="py-12">
-                <Hero />
-            </section>
-            <section id="about" className="py-12">
-                <About />
-            </section>
-            <section id="skills" className="py-12">
-                <Skills />
-            </section>
-            <section id="projects" className="py-12">
-                <Projects />
-            </section>
-            <section id="experience" className="py-12">
-                <Experience />
-            </section>
-            <section id="contact" className="py-12">
-                <Contact />
-            </section>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <section id="home" className="py-12">
+        <Hero />
+      </section>
+      <section id="about" className="py-12">
+        <About />
+      </section>
+      <section id="skills" className="py-12">
+        <Skills />
+      </section>
+      <section id="projects" className="py-12">
+        <Projects />
+      </section>
+      <section id="experience" className="py-12">
+        <Experience />
+      </section>
+      <section id="contact" className="py-12">
+        <Contact />
+      </section>
+      <Footer />
+    </div>
+  );
 }

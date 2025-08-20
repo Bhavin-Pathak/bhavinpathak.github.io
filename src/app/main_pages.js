6ad169ca83cf8a1332f7e3522a7f6042dd747028
Bhavin-Pathak  <bhavinpathak29@gmail.com>
2025-08-20 21:55:25 +0530

    chore: reduce spacing from top

--- src/app/main_pages.js
+++ src/app/main_pages.js
@@ -10,22 +10,22 @@ export default function Main_Pages() {
   return (
     <div className="min-h-screen">
       <Header />
-      <section id="home" className="py-12">
+      <section id="home" className="py-5">
         <Hero />
       </section>
-      <section id="about" className="py-12">
+      <section id="about" className="py-5">
         <About />
       </section>
-      <section id="skills" className="py-12">
+      <section id="skills" className="py-5">
         <Skills />
       </section>
-      <section id="projects" className="py-12">
+      <section id="projects" className="py-5">
         <Projects />
       </section>
-      <section id="experience" className="py-12">
+      <section id="experience" className="py-5">
         <Experience />
       </section>
-      <section id="contact" className="py-12">
+      <section id="contact" className="py-5">
         <Contact />
       </section>
     </div>
